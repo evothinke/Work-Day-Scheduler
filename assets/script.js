ca2b82bcf41647f1398a3abe72239c3b4d83fb07
August Tornea <august.tornea@gmail.com>
2023-04-16 14:08:09 -0400

    finished working on the functions, conditionals.

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,28 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+setInterval(() => $("#currentDay").text(moment().format("dddd, MMM Do, h:mm:ss a")), 1000);
+let currentHour = parseInt(moment().hours());
+var timeBlockHour  = 0;
 
 
-
-var hours =[8, 9, 10,11,12,13,14,15,16,17]
 $(document).ready(function() {
-  timeSlots.forEach(slot => {
-    $("#" + slot).val(localStorage.getItem(slot));
-  });
-  
-  renderPlans();
-
-
- $(".saveBtn").click(function () {
-  //declaring 2 variables to handle the user input and the time it is assiged to
-  var dateTime = $(this).attr()
-var userInput = $("#" + dateTime)
-
- }) 
-
-
-$(function () {
-
-
-
-
+  [8,9,10,11,12,13,14,15,16,17].forEach(field => $("#" + field).val(localStorage.getItem(field)));
 });
 
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
+$(".saveBtn").click(function () {
+  var dataHour = $(this).attr("data-hour");
+  localStorage.setItem(dataHour, $("#" + dataHour).val());
+  console.log(localStorage);
+  console.log($("#" + dataHour).val());
+});
+$(function() {
+  var currentHour = dayjs().hour();
 
+  $(".time-block").each(function() {
+    timeBlockHour = parseInt($(this).attr("id").substring($(this).attr("id").length - 2));
 
+    console.log(timeBlockHour);
 
-
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-
-
-
-
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-
-
-
-// TODO: Add code to display the current date in the header of the page.
+    $(this).toggleClass("past", timeBlockHour < currentHour);
+    $(this).toggleClass("present", timeBlockHour === currentHour);
+    $(this).toggleClass("future", timeBlockHour > currentHour);
+  });
+});
